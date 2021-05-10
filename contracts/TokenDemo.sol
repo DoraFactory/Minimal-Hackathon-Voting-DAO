84cfd3cf2853434031b8b0241072c6a942e7eb6c
lucas7788 <sishsh@163.com>
2021-05-10 10:38:19 +0800

    add token demo

--- contracts/TokenDemo.sol
+++ contracts/TokenDemo.sol
@@ -1,69 +1,27 @@
-pragma solidity ^0.5.16;
+pragma solidity 0.8.0;
 
-import "./Governance/Comp.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenDemo is Token {
+//TODO 仅用于测试
 
-    string public name;                   //名称，例如"My test token"
-    uint8 public decimals;               //返回token使用的小数点后几位。比如如果设置为3，就是支持0.001表示.
-    string public symbol;               //token简称,like MTT
+//SPDX-License-Identifier: <SPDX-License>
+contract TokenDemo is ERC20, Ownable {
 
-    constructor(uint256 _initialAmount, string memory _tokenName, uint8 _decimalUnits, string memory _tokenSymbol) public {
-        totalSupply = _initialAmount * 10 ** uint256(_decimalUnits);
-        // 设置初始总量
-        balances[msg.sender] = totalSupply;
-        // 初始token数量给予消息发送者，因为是构造函数，所以这里也是合约的创建者
-
-        name = _tokenName;
-        decimals = _decimalUnits;
-        symbol = _tokenSymbol;
+    constructor()ERC20("Test Token", "Test"){
+        uint totalSupply = 500000 * (10 ** decimals());
+        _mint(owner(), totalSupply);
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        //默认totalSupply 不会超过最大值 (2^256 - 1).
-        //如果随着时间的推移将会有新的token生成，则可以用下面这句避免溢出的异常
-        require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to], "transfer balance not enough");
-        balances[msg.sender] -= _value;
-        //从消息发送者账户中减去token数量_value
-        balances[_to] += _value;
-        //往接收账户增加token数量_value
-        emit Transfer(msg.sender, _to, _value);
-        //触发转币交易事件
-        return true;
+    function decimals() public pure override returns (uint8){
+        return 9;
     }
 
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns
-    (bool success) {
-        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value, "transferFrom balance not enough");
-        balances[_to] += _value;
-        //接收账户增加token数量_value
-        balances[_from] -= _value;
-        //支出账户_from减去token数量_value
-        allowed[_from][msg.sender] -= _value;
-        //消息发送者可以从账户_from中转出的数量减少_value
-        emit Transfer(_from, _to, _value);
-        //触发转币交易事件
-        return true;
+    function mint(address to, uint amount) public onlyOwner() {
+        _mint(to, amount);
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
+    function burn(uint amount) public {
+        _burn(_msgSender(), amount);
     }
-
-
-    function approve(address _spender, uint256 _value) public returns (bool success)
-    {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-        //允许_spender从_owner中转出的token数
-    }
-
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
 }
